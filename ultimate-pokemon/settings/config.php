c10c2ddfac68b7442601d47f94a24fd7fa891366
Otávio Maldaner <otaviomaldaner571@gmail.com>
2023-03-15 09:05:02 -0300

    upload

--- ultimate-pokemon/settings/config.php
+++ ultimate-pokemon/settings/config.php
@@ -8,7 +8,7 @@
  * Configurações gerais do sistema
  * 
  */
-// require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 const DEFAULT_LANGUAGE = 'pt-br';
 const DATETIME_FORMAT = 'Y-m-d H:i:s';
 const USER_DATE_FORMAT = 'd/m/Y - H:i';
---
+++ ultimate-pokemon/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'otavio.maldaner/ultimate-pokemon',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '67774482540799ae3b59c3452a99d15ca64347e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'otavio.maldaner/ultimate-pokemon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '67774482540799ae3b59c3452a99d15ca64347e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
