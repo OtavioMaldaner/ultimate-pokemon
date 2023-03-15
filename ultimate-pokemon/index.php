67774482540799ae3b59c3452a99d15ca64347e1
Otávio Maldaner <otaviomaldaner571@gmail.com>
2023-03-15 08:47:07 -0300

    upload

---
+++ ultimate-pokemon/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__.'/settings/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/olifx_logo.png" type="image/png">
+    <link rel="stylesheet" href="style.css">
+    <title>OLIFX</title>
+</head>
+<body>
+    <div class="homepage-container">
+        <div class="homepage-util-area">
+            <div class="hp-superior-part">
+                <div class="hp-title">
+                    <h1>Welcome to OLIFX</h1>
+                </div>
+                <div class="hp-subtitle">
+                    <h3>Platform to advertise and sell products among the IFRS Campus Feliz community.</h3>
+                </div>
+            </div>
+            <div class="hp-mid-part">
+                <img src="./assets/images/OLIFX.png" alt="" class="hp-main-image">
+            </div>
+            <div class="hp-bottom-part">
+                <div class="hp-button-space">
+                    <a href="page/login">
+                        <div class="hp-button">
+                            <p>Enter the app</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ ultimate-pokemon/settings/config.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+ * OLIFX - Feliz-RS                |  https://github.com/OtavioMaldaner/ultimate-pokemon
+ * by: Otávio Maldaner             |  https://github.com/OtavioMaldaner
+ *
+ * 2023.03.15 - Feliz - RS
+ *
+ * Configurações gerais do sistema
+ * 
+ */
+// require_once __DIR__.'/../vendor/autoload.php';
+const DEFAULT_LANGUAGE = 'pt-br';
+const DATETIME_FORMAT = 'Y-m-d H:i:s';
+const USER_DATE_FORMAT = 'd/m/Y - H:i';
+const SESSION_LIFETIME = 86400;
