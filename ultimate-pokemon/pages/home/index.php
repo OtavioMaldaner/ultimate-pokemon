b32e2c0ef540a3c455094b6ea8ebe25a0b351d73
Otávio Maldaner <otaviomaldaner571@gmail.com>
2023-03-22 09:05:06 -0300

    carousel-pokemon

--- ultimate-pokemon/pages/home/index.php
+++ ultimate-pokemon/pages/home/index.php
@@ -28,26 +28,91 @@ $pokemons_carteira = Wallet::getPlayerWallet($_SESSION['idPlayer']);
 
 <body>
     <div class="container">
-        <div class="swiper">
-            <div class="swiper-wrapper">
-                <?php
-                    if (count($pokemons_carteira) > 0) {
-                        foreach ($pokemons_carteira as $pokemons) {
-                            $id = $pokemons->getIdPokemon();
-                            $pokemon = Pokemon::find($id);
+        <div class="pokemon-carousel">
+            <button class="arrow-left control" aria-label="Previous image">
+                <img src="../imagens/left_arrow.svg" alt="">
+            </button>
+            <div class="carousel-monitor">
+                <img class="monitor-support" src="../imagens/Monitor 2.svg" alt="">
+                <div class="gallery-wrapper">
+                    <div class="gallery">
+                        <?php
+                        if (count($pokemons_carteira) > 0) {
+                            foreach ($pokemons_carteira as $pokemons) {
+                                $id = $pokemons->getIdPokemon();
+                                $pokemon = Pokemon::find($id);
 
-                            echo "<div class=\"swiper-slide\">";
-                            echo "<h2 class=\"pokemon-type\">".$pokemon->getTipo()."</h2>";
-                            echo "<h1 class=\"pokemon-name\">".$pokemon->getNome()."</h1>";
-                            echo "</div>";
+                                echo "<div class=\"item current-item\">";
+                                echo "<h2 class=\"pokemon-type\">" . $pokemon->getTipo() . "</h2>";
+                                echo "<h1 class=\"pokemon-name\">" . $pokemon->getNome() . "</h1>";
+                                echo "<h3 class=\"pokemon-owner\">" . $_SESSION['nickName'] . "</h3>";
+                                // echo "<img src=\"../../assets/suarez.jpg\" alt=\"\" class=\"pokemon-image\">";
+                                echo "<div class=\"additional-infos\">" . "<span class=\"pokemon-over\">" . $pokemon->getOver() . "</span>" . "<span class=\"pokemon-height\">" . $pokemon->getAltura() . "</span>" . "<span class=\"pokemon-weight\">" . $pokemon->getPeso() . "</span>" . "</div>";
+                                echo "</div>";
+                            }
                         }
-                    }
-                ?>
+                        ?>
+                    </div>
+                </div>
             </div>
+            <button class="arrow-right control" aria-label="Next image">
+                <img src="../imagens/right_arrow.svg" alt="">
+            </button>
+            <div class="mobile-arrows">
+                <div class="control mobile-arrow-left" aria-label="Previous image">
+                    <img src="../imagens/left_arrow.svg" alt="">
+                </div>
+                <div class="control mobile-arrow-right" aria-label="Next image">
+                    <img src="../imagens/right_arrow.svg" alt="">
+                </div>
+            </div>
+
+
+
         </div>
-    </div>
 </body>
 
-<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<script>
+    let screenWidth = window.innerWidth;
+    const controls = document.querySelectorAll('.control');
+    let maxItems;
+    const images = document.querySelectorAll('.item');
+    items = images;
+    const tamanhoArray = items.length;
+    maxItems = tamanhoArray;
+    let currentItem = 0;
+    let interval;
+    items.forEach(item => item.classList.remove('current-item'));
+    items[0].classList.add('current-item');
+    controls.forEach(control => {
+        control.addEventListener('click', (e) => {
+            e.stopPropagation();
+            const isLeft = control.classList.contains('arrow-left');
+            if (isLeft) {
+                currentItem -= 1;
+            } else {
+                currentItem += 1;
+            }
+            if (currentItem >= maxItems) {
+                currentItem = 0;
+            } else if (currentItem < 0) {
+                currentItem = maxItems - 1;
+            }
+            items.forEach(item => item.classList.remove('current-item'));
+            items[currentItem].classList.add('current-item');
+            clearInterval(carouselInterval);
+        })
+    });
+    const carouselInterval = setInterval(function() {
+        currentItem++;
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+        items.forEach(item => item.classList.remove('current-item'));
+        items[currentItem].classList.add('current-item');
+        interval = 3000;
+    }, interval ? interval : 3000);
+</script>
+
 
 </html>
