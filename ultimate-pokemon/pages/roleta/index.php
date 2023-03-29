fb0884ae937c262f1795cff4d4685be4f45adfdc
Matheus Persch <matheusp.estudos@gmail.com>
2023-03-29 15:34:10 -0300

    Update index.php

--- ultimate-pokemon/pages/roleta/index.php
+++ ultimate-pokemon/pages/roleta/index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="br">
-<link rel="stylesheet" href="style.css">
-<head>
-    <title>Sorteador de Pokemons</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press+Start+2P">
-</head>
-<body>
-<div class="container">
-    <h1>Sorteador de Pokemons</h1>
-    <form method="post">
-        <input type="submit" name="sortear" value="Sortear um Pokemon" />
-    </form>
-</div>
-</body>
-</html>
-
 <?php
 
 $pokemons = array("bulbasaur.png", "charmander.png", "squirtle.png", "pikachu.png", "jigglypuff.png", "snorlax.png", "mewtwo.png", "zapdos.png", "articuno.png", "moltres.png", "dragonite.png", "mew.png");
@@ -60,3 +43,22 @@ if (isset($_COOKIE[$cookie_nome])) {
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="br">
+<link rel="stylesheet" href="style.css">
+<head>
+    <title>Sorteador de Pokemons</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press+Start+2P">
+</head>
+<body>
+<div class="container">
+    <h1>Sorteador de Pokemons</h1>
+    <form method="post">
+        <input type="submit" name="sortear" value="Sortear um Pokemon" />
+    </form>
+</div>
+</body>
+</html>
+
+
