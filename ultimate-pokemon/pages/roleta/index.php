a771c5b473d127bfdc9878fbb725c42615c3d892
Matheus Persch <matheusp.estudos@gmail.com>
2023-03-29 15:31:46 -0300

    roleta com cookies e opção do download da imagem

--- ultimate-pokemon/pages/roleta/index.php
+++ ultimate-pokemon/pages/roleta/index.php
@@ -17,26 +17,46 @@
 
 <?php
 
-$pokemons = array(
-    array("nome" => "Bulbasaur", "imagem" => "bulbasaur.png"),
-    array("nome" => "Charmander", "imagem" => "charmander.png"),
-    array("nome" => "Squirtle", "imagem" => "squirtle.png"),
-    array("nome" => "Pikachu", "imagem" => "pikachu.png"),
-    array("nome" => "Jigglypuff", "imagem" => "jigglypuff.png"),
-    array("nome" => "Snorlax", "imagem" => "snorlax.png"),
-    array("nome" => "Mewtwo", "imagem" => "mewtwo.png"),
-    array("nome" => "Zapdos", "imagem" => "zapdos.png"),
-    array("nome" => "Articuno", "imagem" => "articuno.png"),
-    array("nome" => "Moltres", "imagem" => "moltres.png"),
-    array("nome" => "Dragonite", "imagem" => "dragonite.png"),
-    array("nome" => "Mew", "imagem" => "mew.png")
-);
+$pokemons = array("bulbasaur.png", "charmander.png", "squirtle.png", "pikachu.png", "jigglypuff.png", "snorlax.png", "mewtwo.png", "zapdos.png", "articuno.png", "moltres.png", "dragonite.png", "mew.png");
 
-if (isset($_POST['sortear'])) {
+// Definir o nome do cookie
+$cookie_nome = "ultimo_sorteio";
+
+// Definir o tempo de expiração do cookie em segundos (um dia)
+$cookie_expira = 10;
+
+// Verificar se o cookie existe
+if (isset($_COOKIE[$cookie_nome])) {
+    $ultimo_sorteio = $_COOKIE[$cookie_nome];
+    $agora = time();
+
+    // Verificar se já passou um dia desde o último sorteio
+    if (($agora - $ultimo_sorteio) < $cookie_expira) {
+        echo "<p>Você já sorteou hoje. Tente novamente amanhã.</p>";
+    } else {
+        // Sortear um novo pokemon
+        $sorteado = rand(0, count($pokemons) - 1);
+        $pokemon_sorteado = $pokemons[$sorteado];
+        echo "<h2>Parabéns, você sorteou:</h2>";
+        echo "<img src=\"../../database/media/$pokemon_sorteado\" alt=\"Pokemon sorteado\" />";
+        echo "<a href=\"../../database/media/$pokemon_sorteado\" download>Download da imagem</a>";
+
+
+        // Definir o novo valor do cookie com a data atual
+        setcookie($cookie_nome, time(), time() + $cookie_expira);
+    }
+} else {
+    // Sortear um novo pokemon
     $sorteado = rand(0, count($pokemons) - 1);
     $pokemon_sorteado = $pokemons[$sorteado];
-    echo "<h2>" . $pokemon_sorteado['nome'] . "</h2>";
-    echo "<img src=\"../../database/media/" . $pokemon_sorteado['imagem'] . "\" alt=\"Pokemon sorteado\" />";
+    echo "<h2>Parabéns, você sorteou:</h2>";
+    echo "<img src=\"../../database/media/$pokemon_sorteado\" alt=\"Pokemon sorteado\" />";
+    echo "<a href=\"../../database/media/$pokemon_sorteado\" download>Download da imagem</a>";
+
+
+    // Definir o valor do cookie com a data atual
+    setcookie($cookie_nome, time(), time() + $cookie_expira);
 }
+
 
 ?>
